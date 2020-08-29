c848a7b6e520f9e0019a895bd25656656ce8ac44
Hanson <hansonho61004@gmail.com>
2020-08-29 09:04:00 +0800

    1. 更換跑馬燈文字

--- Fishery/ClientApp/src/components/Home/marquee.js
+++ Fishery/ClientApp/src/components/Home/marquee.js
@@ -1,10 +1,9 @@
 import React from 'react';
-
 function Marquee() {
     return (
         <div className="marquee-info">
             <div className="item">
-                <span>歡迎蒞臨深澳漁港</span>
+                <span>[109年國家災防日演習]地震速報演練，臨震應變「趴下、掩護、穩住[Earthquake Disaster Drill]」</span>
             </div>
         </div>
     );
