0b18cff54bf2a5703a41dbd559417381939212a5
backtolive@gmail.com <backtolive@gmail.com>
2020-11-30 20:57:44 +0800

    後台餐廳 菜單 船班 完成
    剩景點管理沒完成

---
+++ Fishery/Areas/Admin/Controllers/BoatController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fishery.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fishery.Areas.Admin.Controllers
+{
+    public class BoatController : Controller
+    {
+        private readonly FisheryContext _context;
+
+        public BoatController(FisheryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Boat.ToList();
+            return View(model);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View(_context.Boat.Find(id));
+        }
+
+        public IActionResult Create()
+        {
+            return View("~/Areas/Admin/Views/Boat/Edit.cshtml", new Boat());
+        }
+
+        [HttpPost]
+        public IActionResult Save(Boat model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Boat.Update(model);
+                _context.SaveChanges();
+            }
+            else
+            {
+                return View("~/Areas/Admin/Views/Boat/Edit.cshtml", model);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Fishery/Areas/Admin/Controllers/FoodController.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Fishery.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fishery.Areas.Controllers
+{
+    public class FoodController : Controller
+    {
+        private readonly IHostingEnvironment _env;
+        private readonly FisheryContext _context;
+
+        public FoodController(IHostingEnvironment env, FisheryContext context)
+        {
+            _env = env;
+            _context = context;
+        }
+
+        /// <summary>
+        /// 預設是餐廳管理
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            var model = _context.Food.OrderBy(a => a.Seq).ToList();
+            return View(model);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var model = _context.Food.Find(id);
+            return View(new FoodPageModel() { food = model });
+        }
+
+        public IActionResult Create()
+        {
+            return View("~/Areas/Admin/Views/Food/Edit.cshtml",new FoodPageModel() { food = new Food() { Id = 0} });
+        }
+
+        [HttpPost]
+        public IActionResult Save(FoodPageModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var food = _context.Food.Find(model.food.Id);
+                if (model.food.Id != 0)
+                {
+                    if (food == null) return Content("資料錯誤");
+                    else
+                    {
+                        food.Name = model.food.Name;
+                        food.Desc = model.food.Desc;
+                        food.Seq = model.food.Seq;
+                        food.UpdateTime = DateTime.Now;
+                    }
+                }
+                else
+                {
+                    food = model.food;
+                    food.UpdateTime = DateTime.Now;
+                    _context.Food.Add(food);
+                }
+
+                if (model.coverImage != null)
+                {
+                    //有選擇檔案, 先檢查檔名
+                    string filename = model.coverImage.FileName;
+                    var targetFile = Path.Combine(_env.WebRootPath, "images", "foods", model.coverImage.FileName);
+                    int index = 1;
+
+                    while (System.IO.File.Exists(targetFile))
+                    {
+                        //改名改到不同名稱為止
+                        filename = String.Format("{0}_{1}.{2}",
+                            Path.GetFileNameWithoutExtension(model.coverImage.FileName),
+                            index,
+                            Path.GetExtension(model.coverImage.FileName));
+                        targetFile = Path.Combine(_env.WebRootPath, "images", "foods", filename);
+                        index++;
+                    }
+                    model.coverImage.CopyTo(new FileStream(targetFile, FileMode.Create));
+                    model.food.Cover = filename;
+                    food.Cover = filename;
+                }
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View("~/Areas/Admin/Views/Manage/Edit.cshtml", model.food.Id);
+        }
+
+        /// <summary>
+        /// 列出指定餐廳的菜單
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IActionResult Menu(int id)
+        {
+            var food = _context.Food.Find(id);
+            if (food == null)
+            {
+                return Content("輸入錯誤");
+            }
+            ViewData["name"] = food.Name;
+            ViewData["foodid"] = food.Id;
+            var menu = _context.FoodMenu.Where(a => a.FoodId == id).ToList();
+
+            return View(menu);
+        }
+
+        public IActionResult MenuEdit(int id)
+        {
+            var menu = _context.FoodMenu.Find(id);
+            if (menu == null) return Content("資料錯誤.");
+
+            FoodMenuPageModel model = new FoodMenuPageModel()
+            {
+                 menu = menu
+            };
+            return View(model);
+        }
+
+        public IActionResult MenuCreate(int id)
+        {
+            FoodMenuPageModel model = new FoodMenuPageModel() { 
+                menu = new FoodMenu() { 
+                     Id = 0,
+                     FoodId = id
+                }
+            };
+            return View("~/Areas/Admin/Views/Manage/MenuEdit.cshtml", model);
+        }
+
+        public IActionResult MenuSave(FoodMenuPageModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var menu = _context.FoodMenu.Find(model.menu.Id);
+                if (model.menu.Id != 0)
+                {
+                    if (menu == null) return Content("資料錯誤");
+                    else
+                    {
+                        menu.Name = model.menu.Name;
+                        menu.Desc = model.menu.Desc;
+                    }
+                }
+                else
+                {
+                    menu = model.menu;
+                    _context.FoodMenu.Add(menu);
+                }
+
+
+                if (model.pic != null)
+                {
+                    //有選擇檔案, 先檢查檔名
+                    string filename = model.pic.FileName;
+                    var targetFile = Path.Combine(_env.WebRootPath, "images", "foods", model.pic.FileName);
+                    int index = 1;
+
+                    while (System.IO.File.Exists(targetFile))
+                    {
+                        //改名改到不同名稱為止
+                        filename = String.Format("{0}_{1}.{2}",
+                            Path.GetFileNameWithoutExtension(model.pic.FileName),
+                            index,
+                            Path.GetExtension(model.pic.FileName));
+                        targetFile = Path.Combine(_env.WebRootPath, "images", "foods", filename);
+                        index++;
+                    }
+                    model.pic.CopyTo(new FileStream(targetFile, FileMode.Create));
+                    model.menu.Pic1 = filename;
+                    menu.Pic1 = filename;
+                }
+                _context.SaveChanges();
+                return RedirectToAction(actionName: "Menu", routeValues: new { id = model.menu.FoodId });
+            }
+            else
+            { 
+                return View("~/Areas/Admin/Views/Manage/MenuEdit.cshtml", model); 
+            }
+            
+        }
+
+    }
+
+    public class FoodPageModel
+    {
+        public Food food { get; set; }
+        public IFormFile coverImage { set; get; }
+    }
+
+    public class FoodMenuPageModel
+    { 
+        public FoodMenu menu { get; set; }
+
+        public IFormFile pic { get; set; }
+    }
+}
